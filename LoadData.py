87686a5399d1ae6ed6be684fe71274b41fe2d6f8
Sameer Kumar <43317325+bvsk35@users.noreply.github.com>
2018-11-25 13:03:39 -0800

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -26,6 +26,18 @@ def load_test_labels(filename):
     with gzip.open(filename) as f:
         a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=8)
         return a
+
+# Function for loading images and labels can be clubed into one function
+def load_images(filename):
+    with gzip.open(filename) as f:
+        a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=16).reshape(-1, 784)
+        return a
+
+def load_labels(filename):
+    with gzip.open(filename) as f:
+        a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=8)
+        return a
+
 def InitialWeights():
     W_Layer_2 = numpy.random.normal(0, 0.050507, size=(40, 785))
     W_Layer_3 = numpy.random.normal(0, 0.44721, size=(10, 41))
