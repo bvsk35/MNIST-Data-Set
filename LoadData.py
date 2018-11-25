3a0f736604e8718353d47bdabdba47573a509e4f
Sameer Kumar <43317325+bvsk35@users.noreply.github.com>
2018-11-25 13:11:09 -0800

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -7,25 +7,25 @@ import gzip
 import numpy
 
 # Functions
-def load_training_images(filename):
-    with gzip.open(filename) as f:
-        a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=16).reshape(-1, 784)
-        return a
+# def load_training_images(filename):
+#     with gzip.open(filename) as f:
+#         a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=16).reshape(-1, 784)
+#         return a
 
-def load_training_labels(filename):
-    with gzip.open(filename) as f:
-        a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=8)
-        return a
+# def load_training_labels(filename):
+#     with gzip.open(filename) as f:
+#         a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=8)
+#         return a
 
-def load_test_images(filename):
-    with gzip.open(filename) as f:
-        a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=16).reshape(-1, 784)
-        return a
+# def load_test_images(filename):
+#     with gzip.open(filename) as f:
+#         a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=16).reshape(-1, 784)
+#         return a
 
-def load_test_labels(filename):
-    with gzip.open(filename) as f:
-        a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=8)
-        return a
+# def load_test_labels(filename):
+#     with gzip.open(filename) as f:
+#         a = numpy.frombuffer(f.read(), dtype=numpy.uint8, offset=8)
+#         return a
 
 # Function for loading images and labels can be clubed into one function
 def load_images(filename):
